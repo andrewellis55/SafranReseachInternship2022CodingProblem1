5894fc0551c0bf18ae70771d00858d8a583c5c63
Andrew Ellis <andrew.ellis@safrangroup.com>
2022-05-31 17:05:42 -0400

    initial commit

---
+++ newton_minimize.m
@@ -0,0 +1,14 @@
+function [x_k] = newton_minimize(f, fp, x0)
+  % parameters
+  % ----------
+  % f: function
+  % fp: first derivative of the function
+  % x0: starting point for the iteration
+
+  % returns
+  % -------
+  % x_k: the value of x at the minima
+  
+  % YOUR CODE HERE
+end
+
